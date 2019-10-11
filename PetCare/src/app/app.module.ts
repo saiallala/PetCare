6031d3a13ee6463950bbc2cdc1261aeeebe0e67e
Sai Allala <skrallala752@gmail.com>
2019-10-11 15:34:12 -0400

    New components added, still needs to work on them componets

--- PetCare/src/app/app.module.ts
+++ PetCare/src/app/app.module.ts
@@ -1,14 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AccountComponent } from './account/account.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HelpComponent } from './help/help.component';
+import { Routes,RouterModule } from '@angular/router';
+import { PersonalComponent } from './profile/personal/personal.component';
+import { HealthComponent } from './profile/health/health.component';
+import { ContactInfoComponent } from './profile/contact-info/contact-info.component';
+import {FormsModule} from '@angular/forms';
+import { ViewAccountComponent } from './account/view-account/view-account.component';
+import { CreateAccountComponent } from './account/create-account/create-account.component';
+
+
+
+const appRoutes: Routes =[
+        { path: '', component: HomeComponent },
+        { path: 'profile', component: ProfileComponent },
+        { path: 'help', component: HelpComponent },
+        { path: 'account', component: AccountComponent },
+        { path: 'viewAccount', component: ViewAccountComponent },
+        { path: 'createAccount', component: CreateAccountComponent },
+        { path: 'health', component: HealthComponent },
+        { path: 'contact', component: ContactComponent }
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AccountComponent,
+    ProfileComponent,
+    HelpComponent,
+    HealthComponent,
+    ContactInfoComponent,
+    ViewAccountComponent,
+    CreateAccountComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
