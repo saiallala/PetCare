a13d3bcc59848f61a45cfeed3dec50ec9338ad6e
Sai Allala <skrallala752@gmail.com>
2019-10-29 16:35:46 -0400

    Better Header oage

--- PetCare/src/app/app.module.ts
+++ PetCare/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule, MatDialog, MatCheckboxModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,25 +12,26 @@ import { ContactComponent } from './contact/contact.component';
 import { AccountComponent } from './account/account.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HelpComponent } from './help/help.component';
-import { Routes,RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { HealthComponent } from './profile/health/health.component';
 import { ContactInfoComponent } from './profile/contact-info/contact-info.component';
-import {FormsModule} from '@angular/forms';
-import { ViewAccountComponent } from './account/view-account/view-account.component';
 import { CreateAccountComponent } from './account/create-account/create-account.component';
 import { PetService } from './services/pet.service';
+import { LoginComponent } from './login/login.component';
+import { IntroComponent } from './intro/intro.component';
 
 
 
-const appRoutes: Routes =[
-        { path: '', component: HomeComponent },
-        { path: 'profile', component: ProfileComponent },
-        { path: 'help', component: HelpComponent },
-        { path: 'account', component: AccountComponent },
-        { path: 'view-account', component: ViewAccountComponent },
-        { path: 'create-account', component: CreateAccountComponent },
-        { path: 'health', component: HealthComponent },
-        { path: 'contact', component: ContactInfoComponent }
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'help', component: HelpComponent },
+  { path: 'account', component: AccountComponent },
+  { path: 'register', component: CreateAccountComponent },
+  { path: 'health', component: HealthComponent },
+  { path: 'contact', component: ContactInfoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: IntroComponent }
 
 ];
 
@@ -40,17 +45,25 @@ const appRoutes: Routes =[
     HelpComponent,
     HealthComponent,
     ContactInfoComponent,
-    ViewAccountComponent,
     CreateAccountComponent,
+    LoginComponent,
+    IntroComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatToolbarModule
   ],
   exports: [
-      RouterModule
+    RouterModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatToolbarModule
   ],
   providers: [PetService],
   bootstrap: [AppComponent]
